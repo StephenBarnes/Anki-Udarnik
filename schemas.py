bbe2ad2e922ef376bd7eebb22c274b78004aa0e9
J <stbarnes@stanford.edu>
2018-09-08 16:03:44 -0700

    Implement CategoricalPieces for new images+audio, create .gitignore

--- schemas.py
+++ schemas.py
@@ -2,7 +2,6 @@
 
 from aqt.qt import QSplashScreen, QPixmap, QTimer
 from aqt import mw
-from aqt.qt import QSvgRenderer
 
 from random import random
 from os import system
@@ -40,7 +39,6 @@ flashPosition = (1000, 500)
 def show_reinforcement(picture, sound, duration):
     system("mplayer " + sound + " >/dev/null 2>/dev/null &")
     #play_sound(sound) # this doesn't seem to work for some reason
-    #svg = QSVGRenderer
     mw.splash = QSplashScreen(QPixmap(picture))
     mw.splash.move(*flashPosition)
     mw.splash.show()
@@ -49,41 +47,28 @@ def show_reinforcement(picture, sound, duration):
 
 
 # define some reinforcement messages (rm), ie tuples of (image, sound, duration) for the various degrees of reinforcement
-rm0 =  (
-        "/home/j/Anki2/addons21/Udarnik/media/circle.png",
-        "/home/j/Anki2/addons21/Udarnik/media/ding.mp3",
-        250,
-        )
-rm1 = (
-        "/home/j/Anki2/addons21/Udarnik/media/earth.png",
-        "/home/j/Anki2/addons21/Udarnik/media/ChatIncoming.wav",
-        500,
-        )
-rm1_longer = (
-        "/home/j/Anki2/addons21/Udarnik/media/earth.png",
-        "/home/j/Anki2/addons21/Udarnik/media/ChatIncoming.wav",
-        1000,
-        )
-rm2 = (
-        "/home/j/Anki2/addons21/Udarnik/media/star.png",
-        "/home/j/Anki2/addons21/Udarnik/media/CallResume.wav",
-        750,
-        )
-rm2_longer = (
-        "/home/j/Anki2/addons21/Udarnik/media/star.png",
-        "/home/j/Anki2/addons21/Udarnik/media/CallResume.wav",
-        1250,
-        )
-rm3 = (
-        "/home/j/Anki2/addons21/Udarnik/media/unicursal_hexagram.png",
-        "/home/j/Anki2/addons21/Udarnik/media/VoicemailReceived.wav",
-        1100,
-        )
-rm4 = (
-        "/home/j/Anki2/addons21/Udarnik/media/unicursal_hexagram.png",
-        "/home/j/Anki2/addons21/Udarnik/media/ding.mp3", # TODO get a better file?
-        1600,
-        )
+def mario_ding_name(n):
+    R = ".mp3"
+    if n % 3:
+        R = f"{n % 3}ding" + R
+    if n >= 3:
+        R = f"{n // 3}mario" + R
+    return R
+prm = [ # Piece-reinforcement tuples using mario sounds and ding sounds
+    (   f"/home/j/Anki2/addons21/Udarnik/media/PNG_dice/{num}b.png",
+        f"/home/j/Anki2/addons21/Udarnik/media/audio/{mario_ding_name(num)}",
+        num * 250)
+    for num in range(1, 9+1)
+]
+prm_long = [
+    (a, b, c*2) for (a, b, c) in prm
+]
+irm = [ # Intermediate-reinforcement tuples using chip sounds
+    (   f"/home/j/Anki2/addons21/Udarnik/media/PNG_dice/{num}.png",
+        f"/home/j/Anki2/addons21/Udarnik/media/audio/{num}chip.mp3",
+        num * 50)
+    for num in range(1, 9+1)
+]
 
 
 class _1For1(Schema):
@@ -98,7 +83,7 @@ class _1For1(Schema):
         reinforcing = random() < prob
         if reinforcing:
             print(">> reinforcing! prob %s" % prob)
-            show_reinforcement(*rm1)
+            show_reinforcement(*prm[0])
             return (1, prob * (1 - prob))
         else:
             print(">> not reinforcing; prob %s" % prob)
@@ -112,7 +97,7 @@ class _1For1(Schema):
 schemas.append(_1For1())
 
 
-class NForN(Schema):
+class NForNSingles(Schema):
     """Abstract base class for schemas that give pieces in a certain regular pattern wrt reinforcements."""
     name = None
     piece_per_reinf = None
@@ -145,82 +130,82 @@ class NForN(Schema):
     def status_output(self):
         return "idx %d" % self.idx
 
-class _4For3(NForN):
-    name = "4 for 3"
+class _4For3Singles(NForNSingles):
+    name = "4 for 3 singles"
     piece_per_reinf = 4. / 3.
-    reinf_sequence = [rm1, rm1, rm2]
-schemas.append(_4For3())
+    reinf_sequence = [prm[0], prm[0], prm[1]]
+schemas.append(_4For3Singles())
 
-class _4For9(NForN):
-    name = "4 for 9"
+class _4For9Singles(NForNSingles):
+    name = "4 for 9 singles"
     piece_per_reinf = 4. / 9.
     reinf_sequence = [
-            rm0, rm0, rm1,
-            rm0, rm0, rm1,
-            rm0, rm0, rm2,
+            irm[0], irm[0], prm[0],
+            irm[0], irm[0], prm[0],
+            irm[0], irm[0], prm[1],
             ]
-schemas.append(_4For9())
+schemas.append(_4For9Singles())
 
-class _13For9(NForN):
-    name = "13 for 9"
+class _13For9Singles(NForNSingles):
+    name = "13 for 9 singles"
     piece_per_reinf = 13. / 9.
     reinf_sequence = [
-            rm1, rm1, rm2,
-            rm1, rm1, rm2,
-            rm1, rm1, rm3,
+            prm[0], prm[0], prm[1],
+            prm[0], prm[0], prm[1],
+            prm[0], prm[0], prm[2],
             ]
-schemas.append(_13For9())
+schemas.append(_13For9Singles())
 
-class _4For27(NForN):
-    name = "4 for 27"
+class _4For27Singles(NForNSingles):
+    name = "4 for 27 singles"
     piece_per_reinf = 4. / 27.
     reinf_sequence = [
-            rm0, rm0, rm0,
-            rm0, rm0, rm0,
-            rm0, rm0, rm1_longer,
+            irm[0], irm[0], irm[0],
+            irm[0], irm[0], irm[0],
+            irm[0], irm[0], prm_long[0],
 
-            rm0, rm0, rm0,
-            rm0, rm0, rm0,
-            rm0, rm0, rm1_longer,
+            irm[0], irm[0], irm[0],
+            irm[0], irm[0], irm[0],
+            irm[0], irm[0], prm_long[0],
 
-            rm0, rm0, rm0,
-            rm0, rm0, rm0,
-            rm0, rm0, rm2_longer,
+            irm[0], irm[0], irm[0],
+            irm[0], irm[0], irm[0],
+            irm[0], irm[0], prm_long[1],
             ]
-schemas.append(_4For27())
+schemas.append(_4For27Singles())
 
 
-class _42For27(NForN):
-    name = "42 for 27"
+class _42For27Singles(NForNSingles):
+    name = "42 for 27 singles"
     piece_per_reinf = 42. / 27.
     reinf_sequence = [
-            rm1, rm1, rm2, # sums to 4
-            rm1, rm1, rm2,
-            rm1, rm1, rm3, # cumulative 13
+            prm[0], prm[0], prm[1], # sums to 4
+            prm[0], prm[0], prm[1],
+            prm[0], prm[0], prm[2], # cumulative 13
 
-            rm1, rm1, rm2,
-            rm1, rm1, rm2,
-            rm1, rm1, rm3, # cumulative 26
+            prm[0], prm[0], prm[1],
+            prm[0], prm[0], prm[1],
+            prm[0], prm[0], prm[2], # cumulative 26
 
-            rm1, rm1, rm3,
-            rm1, rm1, rm3,
-            rm1, rm1, rm4, # cumulative 42
+            prm[0], prm[0], prm[2],
+            prm[0], prm[0], prm[2],
+            prm[0], prm[0], prm[3], # cumulative 42
             ]
-schemas.append(_42For27())
+schemas.append(_42For27Singles())
 
 
-class _1For9(NForN):
-    name = "1 for 9"
+class _1For9Singles(NForNSingles):
+    name = "1 for 9 singles"
     piece_per_reinf = 1. / 9.
     reinf_sequence = [
-            rm0, rm0, rm0,
-            rm0, rm0, rm0,
-            rm0, rm0, rm1_longer,
+            irm[0], irm[0], irm[0],
+            irm[0], irm[0], irm[0],
+            irm[0], irm[0], prm_long[0],
             ]
-schemas.append(_1For9())
+schemas.append(_1For9Singles())
 
 
-class Categorical(Schema):
+class CategoricalPieces(Schema):
     """Abstract base class for schemas that given a number of pieces determined by a geometric distribution."""
     name = None
     piece_probs = None
@@ -285,29 +270,138 @@ class Categorical(Schema):
         pass
 
     def status_output(self):
-        return "(categorical, no state)"
+        return "(categoricalPieces, no state)"
 
-class _2For1(Categorical):
+class _2For1(CategoricalPieces):
     name = "2 for 1"
-    piece_probs = [(1., 2., rm2)]
+    piece_probs = [(1., 2., prm[1])]
 schemas.append(_2For1())
 
-class ApproximateGeometric12(Categorical):
-    name = "Approximate-geometric, factor 1/2"
-    piece_probs = [(8, 1, rm1), (4, 2, rm2), (2, 3, rm3), (1, 4, rm4)]
-schemas.append(ApproximateGeometric12())
+class Geometric12(CategoricalPieces):
+    name = "Geometric, factor 1/2"
+    piece_probs = [(2**(8 - i), i+1, prm[i]) for i in range(0, 8+1)]
+schemas.append(Geometric12())
 
-class ApproximateGeometric13(Categorical):
-    name = "Approximate-geometric, factor 1/3"
-    piece_probs = [(27, 1, rm1), (9, 2, rm2), (3, 3, rm3), (1, 4, rm4)]
-schemas.append(ApproximateGeometric13())
+class Geometric13(CategoricalPieces):
+    name = "Geometric, factor 1/3"
+    piece_probs = [(3**(8 - i), i+1, prm[i]) for i in range(0, 8+1)]
+schemas.append(Geometric13())
 
-class Uniform1234(Categorical):
+class Geometric14(CategoricalPieces):
+    name = "Geometric, factor 1/4"
+    piece_probs = [(4**(8 - i), i+1, prm[i]) for i in range(0, 8+1)]
+schemas.append(Geometric14())
+
+class Geometric23(CategoricalPieces):
+    name = "Geometric, factor 2/3"
+    piece_probs = [((1.5)**(8 - i), i+1, prm[i]) for i in range(0, 8+1)]
+schemas.append(Geometric23())
+
+class Uniform1234(CategoricalPieces):
     name = "Uniform 1-4"
-    piece_probs = [(1, 1, rm1), (1, 2, rm2), (1, 3, rm3), (1, 4, rm4)]
+    piece_probs = [(1, 1, prm[0]), (1, 2, prm[1]), (1, 3, prm[2]), (1, 4, prm[3])]
 schemas.append(Uniform1234())
+
+class CategoricalPartials(Schema):
+    """Abstract base class for schemas that given a number of partial pieces determined by a geometric distribution."""
+    name = None
+    partials_probs = None
+        # list of tuples (probability, number of partials)
+    partials_per_piece = None
+        # how many partials constitutes one piece
+
+    def __init__(self):
+        print("Initializing %s" % self.name)
+
+        self.partials_curr = 0
+        self.prev_partials_given = 0
+
+        # normalize self.partials_probs
+        assert self.partials_probs
+        total_prob = 0.
+        for prob, num in self.partials_probs:
+            total_prob += prob
+        for i, (prob, num) in enumerate(self.partials_probs):
+            self.partials_probs[i] = (prob / total_prob, num)
+        print("Normalized partials_probs: %s" % [(prob, num) for (prob, num) in self.partials_probs])
+
+        # compute expected pieces per reinforcement
+        # ie expected number of pieces given, given that there's at least 1 piece given
+        self.expected_piece_per_reinf = 0.
+        for prob, num in self.partials_probs:
+            self.expected_piece_per_reinf += prob * num / self.partials_per_piece
+        print("Expected pieces per reinforcement: %s" % self.expected_piece_per_reinf)
+
+        # Compute variance, as Variance(X) = E[X^2] - E[X]^2
+        # compute E[X^2] first
+        #self.expectation_of_square = 0.
+        #for prob, num in self.partials_probs:
+        #    self.expectation_of_square += prob * (num**2)
+        #self.variance = self.expectation_of_square - (self.expected_piece_per_reinf ** 2)
+
+    def sample_partials_probs(self):
+        r = random()
+        for (prob, num) in self.partials_probs:
+            if prob >= r:
+                return (prob, num)
+            r -= prob
+        assert False
+
+    def card_rev(self, prob):
+        print ("adjusted probability: %s / %s = %s" % (prob, self.expected_piece_per_reinf, prob / self.expected_piece_per_reinf))
+        prob /= self.expected_piece_per_reinf
+        reinforcing = random() < prob
+
+        # Compute variance
+        expected_pieces = prob * self.expected_piece_per_reinf
+        variance = 0.
+        for prob2, num in self.partials_probs: # sum up expected squared deviations from times we do reinforce
+            variance += prob2 * (((num / self.partials_per_piece) - expected_pieces) ** 2)
+        # add squared deviation from when we don't reinforce
+        variance += (1 - prob) * (expected_pieces ** 2)
+
+        if reinforcing:
+            (prob, num) = self.sample_partials_probs()
+            self.prev_partials_given = num
+            self.partials_curr += num
+            if self.partials_curr >= self.partials_per_piece:
+                num_pieces = self.partials_curr // self.partials_per_piece
+                self.partials_curr %= self.partials_per_piece
+                print(">> piece reinforcements given! prob %s, %d pieces now, %d partials given, %d partials left" % (prob, num_pieces, num, self.partials_curr))
+                show_reinforcement(*prm[num_pieces-1])
+            else:
+                print(">> partial reinforcements given; prob %s, %d partials given, %d partials in total" % (prob, num, self.partials_curr))
+                show_reinforcement(*irm[num-1])
+            return (num / self.partials_per_piece, variance)
+        else:
+            self.prev_partials_given = 0
+            print(">> not reinforcing; prob %s, %d partials in total" % (prob, self.partials_curr))
+            return (0, variance)
+
+    def rollback(self):
+        if self.prev_partials_given is None:
+            print("CategoricalPieces can't roll back more than once") #TODO
+        else:
+            self.partials_curr -= self.prev_partials_given
+            self.prev_partials_given = None
+            print("CategoricalPieces rolled back, %d partials now" % self.partials_curr)
+
+    def status_output(self):
+        return "(categorical partials, %d partials currently)" % self.partials_curr
+
+class Geometric2Partials9(CategoricalPartials):
+    name = "2-Geometric partials, 1 for 9"
+    partials_probs = [(2**(8 - i), i+1) for i in range(0, 8+1)]
+    partials_per_piece = 9
+schemas.append(Geometric2Partials9())
+
+class Geometric2Partials3(CategoricalPartials):
+    name = "2-Geometric partials, 1 for 3"
+    partials_probs = [(2**(8 - i), i+1) for i in range(0, 8+1)]
+    partials_per_piece = 3
+schemas.append(Geometric2Partials3())
 
 
 # TODO implement more schemas
+# Eg, one that gives geometrically-distributed *partial* reinforcements
 # for instance, exponential-distribution schemas
-# for instance, geometric-like categorical distribution schemas
